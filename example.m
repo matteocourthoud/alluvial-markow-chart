df46fd7ef80c582db39797aed24b156026ddea76
matteocourthoud <matteo.courthoud@uzh.ch>
2021-02-25 18:04:58 +0100

    update graph

--- example.m
+++ example.m
@@ -4,7 +4,7 @@ Q = [0.6 0.1 0.3
      0.2 0.7 0.1
      0.3 0.3 0.4];
 x = [1, 2, 3, 99, 100];
-ylabels = ["Duopoly", "Mixed", "Monopoly"];
+ylabels = ["State 1", "State 2", "State 3"];
 xlabels = ["1", "2", "3", "\infty", "\infty"];
 title = "State to State Transitions";
 
